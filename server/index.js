15f33707c414b586fac114679075b59178b2dd25
Jamie Smith <thejamiemsmith@gmail.com>
2022-02-28 14:24:33 -0700

    lab complete

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+//Middleware
+app.get("/api/users", (req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends);
+  });
+
+  app.get("/weather/:temperature", (req, res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+    res.status(200).send(phrase);
+  });
+
+const SERVER_PORT = 4000
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on ${SERVER_PORT}`)
+})
