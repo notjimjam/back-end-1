4c21b99e2b7b1d04e7dfe834280182776094aa6e
Jamie Smith <thejamiemsmith@gmail.com>
2022-02-28 14:34:25 -0700

    final lab complete

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 //Middleware
 app.get("/api/users", (req, res) => {
@@ -11,7 +13,8 @@ app.get("/api/users", (req, res) => {
   });
 
   app.get("/weather/:temperature", (req, res) => {
-    const phrase = `<h3>It was ${req.params.temperature} today</h3>`;
+    const { temperature } = req.params;
+    const phrase = `<h3>It was ${temperature} today</h3>`;
     res.status(200).send(phrase);
   });
 
